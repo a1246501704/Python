226067c7d3d04513c5ada93a96c5d0c52a908a79
zhanghongyang <zhy_linux@sina.com>
2020-02-27 14:25:16 +0800

    list 遍历

--- python3基础/2.Python修炼第二层/4 列表类型.py
+++ python3基础/2.Python修炼第二层/4 列表类型.py
@@ -132,7 +132,7 @@ print(l)
 [{'name': 'egon', 'age': 18}, {'name': 'oldboy', 'age': 73}, {'name': 'alex', 'age': 84}]
 '''
 
-# 去除重复
+# 去除重复、去重
 比较容易记忆的是用内置的set
 l1 = ['b','c','d','b','c','a','a'] 
 l2 = list(set(l1)) 
@@ -143,7 +143,7 @@ l1 = ['b','c','d','b','c','a','a']
 l2 = {}.fromkeys(l1).keys() 
 print(l2)
 
-这两种都有个缺点，祛】去除重复元素后排序变了：
+这两种都有个缺点,去除重复元素后排序变了：
 ['a', 'c', 'b', 'd']
 
 如果想要保持他们原来的排序：
@@ -175,9 +175,45 @@ print(l2)
 这样就可以保证排序不变了：['b', 'c', 'd', 'a']
 
 \循环
+方法一：最简单常用的，用for遍历列表
+list = [2, 3, 4]
+for num in list:
+    print (num)
 
+方法二：利用python内置函数enumerate（）列举出list中的数
+enumerate(sequence, [start=0])，返回枚举对象
 
+参数
+    sequence -- 一个序列、迭代器或其他支持迭代对象。
+    start -- 下标起始位置。
 
+list = [2, 3, 4]
+for i in enumerate(list):
+    print （i）
+
+方法三：使用iter（）迭代器
+iter(object[, sentinel]) 函数用来生成迭代器，返回迭代对象。
+参数
+    object -- 支持迭代的集合对象。
+    sentinel -- 如果传递了第二个参数，则参数 object 必须是一个可调用的对象（如，函数），此时，iter 创建了一个迭代器对象，每次调用这个迭代器对象的__next__()方法时，都会调用 object。
+
+用法实例
+list = [2, 3, 4]
+for i in iter(list):
+    print (i)
+
+方法四：使用range（）函数
+pytho range(start, stop[, step]) 函数返回类型是ndarray，可用list（）返回一个整数列表，一般用在 for 循环中。
+
+参数
+    start: 计数从 start 开始。默认是从 0 开始。例如range（5）等价于range（0， 5）;
+    end: 计数到 end 结束，但不包括 end。例如：range（0， 5） 是[0, 1, 2, 3, 4]没有5
+    step：步长，默认为1。例如：range（0， 5） 等价于 range(0, 5, 1)
+用法实例
+list = [2, 3, 4]
+for i in range(len(list)):
+    print i,list[i]
+    
 \练习:
 # 队列（吃了拉）：先进先出,用列表模拟队列
 append，pop
