77a40f73c73e487189d3a6653abd3a37d62c1cb8
carson <zhang.hongyang@mydreamplus.com>
2020-03-12 23:54:59 +0800

    update

--- python3基础/12.Python修炼第十二层/5.CSS选择器.py
+++ python3基础/12.Python修炼第十二层/5.CSS选择器.py
@@ -6,7 +6,7 @@
     # 通配符选择器
 
 \1、id（类似与关键字）选择器
-#1、作用：根据指定的id名称，在当前界面中找到对应的唯一一个的标签，然后设置属性。命名id时避免使用html中的其他标签名称或关键字。
+#1、作用：根据指定的id名称，在当前界面中找到对应的使用了此id名称的标签，然后设置属性。命名id时避免使用html中的其他标签名称或关键字。
 
 #2、格式
 id名称 {
@@ -63,28 +63,26 @@ id名称 {
 # 示例
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-    <title>id选择器</title>
-    <style>
-        .p1 {
-            color: red;
-        }
-        .p2 {
-            color: green;
-        }
-        .p3 {
-            color: blue;
-        }
-    </style>
-</head>
-
-<body>
-<p class="p1">大多数人的帅，都是浮在表面的，是外表的帅</p>
-<p class="p2">而EGON，不仅具备外表帅，内心更是帅了一逼</p>
-<p class="p3">EGON就是我，我就是EGON</p>
-</body>
-
+    <head>
+    <meta charset="utf-8">
+        <title>id选择器</title>
+        <style>
+            .p1 {
+                color: red;
+            }
+            .p2 {
+                color: green;
+            }
+            .p3 {
+                color: blue;
+            }
+        </style>
+    </head>
+    <body>
+        <p class="p1">大多数人的帅，都是浮在表面的，是外表的帅</p>
+        <p class="p2">而EGON，不仅具备外表帅，内心更是帅了一逼</p>
+        <p class="p3">EGON就是我，我就是EGON</p>
+    </body>
 </html>
 
 
@@ -95,29 +93,27 @@ id名称 {
 
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-    <title>id选择器</title>
-    <style>
-        .p1 {
-            color: red;
-        }
-        .p2 {
-            font-size: 50px;
-        }
-        .p3 {
-            text-decoration: underline;
-        }
+    <head>
+    <meta charset="utf-8">
+        <title>id选择器</title>
+        <style>
+            .p1 {
+                color: red;
+            }
+            .p2 {
+                font-size: 50px;
+            }
+            .p3 {
+                text-decoration: underline;
+            }
 
-    </style>
-</head>
-
-<body>
-<p class="p1 p2">第一行内容</p>
-<p class="p2 p3">第二行内容</p>
-<p class="p1 p3">第三行内容</p>
-</body>
-
+        </style>
+    </head>
+    <body>
+        <p class="p1 p2">第一行内容</p>
+        <p class="p2 p3">第二行内容</p>
+        <p class="p1 p3">第三行内容</p>
+    </body>
 </html>
 
 
@@ -138,33 +134,32 @@ id名称 {
 # 示例
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>标签选择器</title>
+    <head>
+        <meta charset="UTF-8">
+        <title>标签选择器</title>
 
-    <style type="text/css">
-        p {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-    <p>zhy美丽的外表下其实隐藏着一颗骚动的心</p>
-    <ul>
-        <li>
-            <ul>
-                <li>
-                    <ul>
-                        <li>
-                            <p>这颗心叫做七巧玲珑心，男人吃了会流泪，女人吃了会怀孕</p>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-    </ul>
-
-</body>
+        <style type="text/css">
+            p {
+                color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <p>zhy美丽的外表下其实隐藏着一颗骚动的心</p>
+        <ul>
+            <li>
+                <ul>
+                    <li>
+                        <ul>
+                            <li>
+                                <p>这颗心叫做七巧玲珑心，男人吃了会流泪，女人吃了会怀孕</p>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </body>
 </html>
 
 \4、通配符选择器
@@ -177,34 +172,28 @@ id名称 {
 
 #3、注意点：
 1、在企业开发中一般不会使用通配符选择器
-    理由是：
-    由于通配符选择器是设置界面上所有的标签的属性，
-    所以在设置之前会遍历所有的标签
-    如果当前界面上的标签比较多，那么性能就会比较差
+    理由是：由于通配符选择器是设置界面上所有的标签的属性，所以在设置之前会遍历所有的标签。如果当前界面上的标签比较多，那么性能就会比较差。
 
 # 示例
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>通配符选择器</title>
-
-    <style type="text/css">
-
-        * {
-            color: red;
-        }
-
-    </style>
-</head>
-<body>
-    <h1 >我是标题</h1>
-    <p >我是段落</p>
-    <a href="#">我是超链接</a>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>通配符选择器</title>
+        <style type="text/css">
+            * {
+                color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>我是标题</h1>
+        <p>我是段落</p>
+        <a href="#">我是超链接</a>
+    </body>
 </html>
 
-\二 组合选择器
+\二 层级选择器
 \1、后代选择器
 #1、作用：找到指定标签的所有后代（儿子，孙子，重孙子、、、）标签，设置属性
 # 修改某个标签或id下的多个同级和子级下的某个级别的样式
@@ -219,56 +208,57 @@ id名称 {
 2、后代不仅仅是儿子，也包括孙子、重孙子
 3、后代选择器不仅仅可以使用标签名称，还可以使用其他选择器比如id或class
 4、后代选择器可以通过空格一直延续下去
+5、如果有多个选择器匹配的相同的标签，以最长的优先。
 
 # 示例
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>后代选择器</title>
+    <head>
+        <meta charset="UTF-8">
+        <title>后代选择器</title>
 
-    <style type="text/css">
-        /*
-        .part1 p  {
-            color: red;
-        }
-        */
-        div p {
-            color: red;
-        }
-        #id1 li p {
-            font-size: 50px;
-        }
+        <style type="text/css">
+            # /*
+            # .part1 p  {
+            #     color: red;
+            # }
+            # */
+            div p {
+                color: red;
+            }
+            # #id1 li p {
+            #     font-size: 50px;
+            # }
 
-        div ul li a {
-            font-size: 100px;
-            color: green;
-        }
-    </style>
-</head>
-<body>
-    <p>我是body下的段落1</p>
-    <!--如果想为div内所有标签都设置属性，无论用id还是class都不合理，因为当div内的标签过多，我们无法加那么多id或者class-->
-    <div id="id1" class="part1">
-        <p>我是div下的段落1</p>
-        <p>我是div下的段落2</p>
-        <ul>
-            <li class="aaa">
-                <p class="ccc">我是ul>li下的段落1</p>
-                <p class="ddd">我是ul>li下的段落</p>
-                <a href="">点我啊1</a>
-            </li>
-            <li>
-                <a href="#">点我啊2</a>
-            </li>
-        </ul>
-    </div>
-    <p>我是body下的段落2</p>
-</body>
+            # div ul li a {
+            #     font-size: 100px;
+            #     color: green;
+            # }
+        </style>
+    </head>
+    <body>
+        <p>我是body下的段落1</p>
+        <!--如果想为div内所有标签都设置属性，无论用id还是class都不合理，因为当div内的标签过多，我们无法加那么多id或者class-->
+        <div id="id1" class="part1">
+            <p>我是div下的段落1</p>
+            <p>我是div下的段落2</p>
+            <ul>
+                <li class="aaa">
+                    <p class="ccc">我是ul>li下的段落1</p>
+                    <p class="ddd">我是ul>li下的段落</p>
+                    <a href="">点我啊1</a>
+                </li>
+                <li>
+                    <a href="#">点我啊2</a>
+                </li>
+            </ul>
+        </div>
+        <p>我是body下的段落2</p>
+    </body>
 </html>
 
-\2、子元素选择器
-#1、作用：找到指定标签的所有特定的直接子元素，然后设置属性。
+\2、子元素选择器（儿子选择器）
+#1、作用：找到指定标签下的所有特定的直接子元素（不匹配子子元素），然后设置属性。
 
 #2、格式：
     标签名称1>标签名称2 {
@@ -278,7 +268,7 @@ id名称 {
 
 #3、注意：
 1、子元素选择器之间需要用>符号链接，并且不能有空格
-    比如div >p会找div标签的所有后代标签，标签名为">p"
+    比如 div>p 会找div标签的所有后代标签，标签名为">p"
 2、子元素选择器只会查找儿子，不会查找其他嵌套的标签
 3、子元素选择器不仅可以用标签名称，还可以使用其他选择器,比如id或class
 4、子元素选择器可以通过 > 符号一直延续下去
@@ -286,50 +276,53 @@ id名称 {
 # 示例
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>后代选择器</title>
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+        <style>
+            /*
+            #id1>p {
+                color: red;
+            }
 
-    <style type="text/css">
-        body>p {
-            color: green;
-        }
-
-        div>p {
-            color: red;
-        }
-
-        .aaa>a {
-            font-size: 100px;
-        }
-
-        div>ul>li>.ddd {
-            color: blue;
-        }
-    </style>
-</head>
-<body>
-    <p>我是body下的段落1</p>
-    <!--如果想为div内所有标签都设置属性，无论用id还是class都不合理，因为当div内的标签过多，我们无法加那么多id或者class-->
-    <div id="id1" class="part1">
-        <p>我是div下的段落1</p>
-        <p>我是div下的段落2</p>
-        <ul>
-            <li class="aaa">
-                <p class="ccc">我是ul>li下的段落1</p>
-                <p class="ddd">我是ul>li下的段落2</p>
-                <a href="">点我啊1</a>
-            </li>
-            <li>
-                <a href="#">点我啊2</a>
-            </li>
-        </ul>
-    </div>
-    <p>我是body下的段落2</p>
-</body>
+            div p {
+                color: red;
+            }
+            */
+            .part1 ul .aaa>a {
+                color: red;
+            }
+        </style>
+    </head>
+    <body>
+      <p>我是body下的段落1</p>
+        <div id="id1" class="part1">
+            <p>我是div下的段落1</p>
+            <p>我是div下的段落2</p>
+            <a href="">点我啊1</a>
+            <ul>
+                <li class="aaa">
+                    <p class="ccc">我是ul>li下的段落1</p>
+                    <p class="ddd">我是ul>li下的段落</p>
+                    <a href="">点我啊1</a>
+                    <p>
+                        <a href="">点我啊2</a>
+                    </p>
+                </li>
+                <li>
+                    <a href="#">点我啊3</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <p>hahahahah</p>
+            <p>hahahahah</p>
+        </div>
+        <p>我是body下的段落2</p>
+      <a href="#">百度一下</a>
+    </body>
 </html>
 
-后代选择器vs子元素选择器
 
 \3、毗邻选择器，CSS2推出（又称相邻兄弟选择器）
 #1、作用：选定紧跟其后的那个标签
@@ -341,9 +334,37 @@ id名称 {
 
 #3、注意点：
 1、毗邻选择器必须通过+号链接，选择器1标签相邻的是选择器2标签才会被选中。
-2、毗邻选择器只能选中紧跟其后的那个标签，不能选中被隔开的标签
+2、毗邻选择器只能选中紧跟其后的那个标签，不能选中被隔开的标签。
 
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>后代选择器</title>
 
+        <style type="text/css">
+            /*相邻兄弟选择器*/
+            h1+p {
+            color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>我是标题1</h1>
+        <a href="">有了这个标签，p就不再是紧跟h1标签了,但通用兄弟选择器仍然能选中</a>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <h1>我是标题2</h1>
+        <p>我是段落</p>
+    </body>
+</html>
 
 \4、弟弟选择器，CSS3推出（又称通用兄弟选择器）
 #1、作用：给指定选择器后面的所有选择器中的所有标签设置属性
@@ -355,42 +376,40 @@ id名称 {
 
 #3、注意点：
 1、通用兄弟选择器必须用~来链接
-2、通用兄弟选择器选中的是指选择器后面的某个选择器选中的所有标签，匹配到 选择器1 下面所有的 选择器2这标签。
-3、无论有没有被隔开，都可以被选中
+2、通用兄弟选择器选中的是指选择器1后面同级的所有 选择器2的标签会被选中，匹配到 选择器1 下面所有的标签为 选择器2标签。
+3、无论有没有被隔开，都可以被选中。非本级的不会被选中。
 
 # 示例
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>后代选择器</title>
+    <head>
+        <meta charset="UTF-8">
+        <title>后代选择器</title>
 
-    <style type="text/css">
-        h1+p {
-            font-size: 50px;
-        }
-        h1~p {
+        <style type="text/css">
+            h1~p {
             color: red;
-        }
-
-    </style>
-</head>
-<body>
-    <h1 >我是标题1</h1>
-    <a href="">有了这个标签，p就不再是紧跟h1标签了,但通用兄弟选择器仍然能选中</a>
-    <p>我是段落</p>
-    <p>我是段落</p>
-    <p>我是段落</p>
-    <p>我是段落</p>
-    <p>我是段落</p>
-    <p>我是段落</p>
-    <p>我是段落</p>
-    <p>我是段落</p>
-    <p>我是段落</p>
-    <h1>我是标题2</h1>
-    <p>我是段落</p>
-
-</body>
+            }
+        </style>
+    </head>
+    <body>
+        <h1 >我是标题1</h1>
+        <a href="">有了这个标签，p就不再是紧跟h1标签了,但通用兄弟选择器仍然能选中</a>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <h1>我是标题2</h1>
+        <p>我是段落</p>
+        <div>
+            <p>我是段落xxxxx</p>
+        </div>
+    </body>
 </html>
 
 
@@ -411,28 +430,27 @@ id名称 {
 # 示例
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>后代选择器</title>
+    <head>
+        <meta charset="UTF-8">
+        <title>后代选择器</title>
 
-    <style type="text/css">
-        p.part1 {
-            color: red;
-        }
-        p#p1{
-            font-size: 100px;
-        }
-    </style>
-</head>
-<body>
-    <p class="part1">我是段落</p>
-    <p id="p1">我是段落</p>
-    <p class="part1">我是段落</p>
-    <p>我是段落</p>
-    <p>我是段落</p>
-    <p>我是段落</p>
-
-</body>
+        <style type="text/css">
+            p.part1 {
+                color: red;
+            }
+            p#p1{
+                font-size: 100px;
+            }
+        </style>
+    </head>
+    <body>
+        <p class="part1">我是段落</p>
+        <p id="p1">我是段落</p>
+        <p class="part1">我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+        <p>我是段落</p>
+    </body>
 </html>
 
 \2、并集选择器
@@ -469,7 +487,6 @@ id名称 {
     <p>我是段落</p>
     <p>我是段落</p>
     <p>我是段落</p>
-
 </body>
 </html>
 
@@ -673,10 +690,10 @@ p:only-child {
 #2、格式与具体用法：
     [属性名]
     其他选择器[属性名]
-    [属性名=值]
+    [属性名=值]  # 等于什么
     [属性名^=值] # 以什么开头
-    [属性名$=值] # 
-    [属性名*=值]
+    [属性名$=值] # 以什么结尾
+    [属性名*=值] # 包含什么
 
 
     例1：找到所有包含id属性的标签
@@ -866,7 +883,7 @@ p:after {
             color: green;
         }
     </style>
-    
+
 </head>
 <body>
     <p>英雄不问出处，流氓不论岁数</p>
@@ -894,43 +911,36 @@ p:after {
 # 示例
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>后代选择器</title>
+    <head>
+        <meta charset="UTF-8">
+        <title>后代选择器</title>
 
-    <style type="text/css">
-
-        div {
-            color: red;
-            font-size: 50px;
-        }
-
-
-    </style>
-</head>
-<body>
-
-<div>
-    <h1>我是标题</h1>
-    <p><a href="#">偶的博爱</a></p>
-    <ul>
-        <li>导航1</li>
-        <li>导航2</li>
-        <li>导航2</li>
-    </ul>
-</div>
-
-<div>
+        <style type="text/css">
+            div {
+                color: red;
+                font-size: 50px;
+            }
+        </style>
+    </head>
+    <body>
     <div>
-        <p>aaaa</p>
+        <h1>我是标题</h1>
+        <p><a href="#">偶的博爱</a></p>
+        <ul>
+            <li>导航1</li>
+            <li>导航2</li>
+            <li>导航2</li>
+        </ul>
     </div>
     <div>
-        <p>bbbb</p>
+        <div>
+            <p>aaaa</p>
+        </div>
+        <div>
+            <p>bbbb</p>
+        </div>
     </div>
-</div>
-
-
-</body>
+    </body>
 </html>
 
 \2、层叠性
